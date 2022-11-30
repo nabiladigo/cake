80552fa79062b5d628090e4c79ac43a1f2945cdd
nabila <nabiladigo91@gmail.com>
2022-11-30 18:21:43 -0500

    adding models

---
+++ models/cake_model.js
@@ -0,0 +1,17 @@
+const cake= [
+    {
+        name:'Vanila cake',
+        price:30,
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg24zArxEdYGlWl5I2_JKssP6SNllGVn-plnxRhbytTMfI14D1YaI8XY6D73Mz4JathOM&usqp=CAU'
+    },
+    {
+        name:'birthday cake',
+        price:30,
+        image:'https://sallysbakingaddiction.com/wp-content/uploads/2022/09/funfetti-birthday-cake.jpg'
+    },
+    {
+        name:'cake',
+        price:30,
+        image:'https://images.pexels.com/photos/1702373/pexels-photo-1702373.jpeg?cs=srgb&dl=pexels-brent-keane-1702373.jpg&fm=jpg'
+    }
+]
--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express= require('express');
 const app =express();
 const PORT= 4000;
 
+const cake= ['napolian', 'rasberry', 'wedding']
 
 app.get('/', (req,res)=>{
     res.send("<h1>Hello World!<h1>")
 })
+app.get('/cake/:cakeIndex', (req, res) => { 
+	res.send(cake[req.params.cakeIndex]);
+});
+
 
 app.listen(PORT, ()=>{
     console.log(`listening for client requests on port ${PORT}`)
