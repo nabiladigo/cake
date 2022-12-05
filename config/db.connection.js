c425a781f82b97a60c4ce6db9162f444fd6a3bba
nabila <nabiladigo91@gmail.com>
2022-12-04 22:28:18 -0500

    adding db

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 // const connectionStr= 'mongo://localhost:27017/cake';
-
+const connectionStr = process.env.MONGODB_URI;
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
