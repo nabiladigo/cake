9e99dc5f9739ee67eff4371f8d97b94d4e7a1b86
nabila <nabiladigo91@gmail.com>
2022-12-20 10:36:30 -0500

    refactering my code

--- models/cake_model.js
+++ models/cake_model.js
@@ -1,27 +1,34 @@
 const mongoose= require('mongoose');
 const cakeSchema= new mongoose.Schema({
-    name:{type: String,},
-    categorie:{type: String},
-    image:{type: String},
+    name:{
+        type: String
+    },
+    categorie:{
+        type: String
+    },
+    image:{
+        type: String
+    },
     price:{
         type: Number,
-        min: [0, 'you can not add a negative number']},
-        direction:{
-            type: String,
-            // required: true
-        },
-        ingredients:{
-            type: String,
-            required: true
-        },
-        cooking:{
-            type:Number,
-            // required:true
-        },
-        prep:{
-            type:Number,
-            // required:true
-        }
+        min: [0, 'you can not add a negative number']
+    },
+    direction:{
+        type: String,
+        // required: true
+    },
+    ingredients:{
+        type: String,
+        required: true
+    },
+    cooking:{
+        type:Number,
+        // required:true
+    },
+    prep:{
+        type:Number,
+        // required:true
+    }
 })
 const Cake = mongoose.model('Cake', cakeSchema);
 module.exports = Cake;
