a59a62a4e6c879cd65270024f40abaf0835c8078
nabila <nabiladigo91@gmail.com>
2022-12-20 22:15:08 -0500

    working on css

--- models/recipes_model.js
+++ models/recipes_model.js
@@ -1,38 +1,38 @@
-const mongoose= require('mongoose');
+// const mongoose= require('mongoose');
 
-const Schema= mongoose.Schema;
+// const Schema= mongoose.Schema;
 
-const recipeSchema= new Schema({
-    name:{
-        type: String, 
-        required: true
-    },
-    image:{
-        type: String,
-        // required: true
-    },
-    direction:{
-        type: String,
-        // required: true
-    },
-    ingredients:{
-        type: String,
-        required: true
-    },
-    cooking:{
-        type:Number,
-        // required:true
-    },
-    prep:{
-        type:Number,
-        // required:true
-    },
-    cake: {
-        type: mongoose.Types.ObjectId,
-        ref: "Cake",
-      }
-});
+// const recipeSchema= new Schema({
+//     name:{
+//         type: String, 
+//         required: true
+//     },
+//     image:{
+//         type: String,
+//         // required: true
+//     },
+//     direction:{
+//         type: String,
+//         // required: true
+//     },
+//     ingredients:{
+//         type: String,
+//         required: true
+//     },
+//     cooking:{
+//         type:Number,
+//         // required:true
+//     },
+//     prep:{
+//         type:Number,
+//         // required:true
+//     },
+//     cake: {
+//         type: mongoose.Types.ObjectId,
+//         ref: "Cake",
+//       }
+// });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+// const Recipe = mongoose.model('Recipe', recipeSchema);
 
-module.exports= Recipe;
+// module.exports= Recipe;
